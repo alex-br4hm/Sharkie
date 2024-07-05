100c66d85ce57ed06d85b211f48c0cce4378f1a1
br4hm <haehnlein.alexander@gmx.de>
2024-07-05 18:32:34 +0200

    new paths

--- models/world.class.js
+++ models/world.class.js
@@ -1,12 +1,10 @@
 class World {
   character = new Character();
-  enemies = [new Pufferfish(), new Pufferfish(), new Pufferfish(), new Jellyfish(), new Jellyfish(), new Jellyfish()];
-  light = new Light();
-  backgroundObject = new BackgroundObject();
-
+  level = level1;
   ctx;
   canvas;
   keyboard;
+  camera_x = 0;
 
   constructor(canvas) {
     this.ctx = canvas.getContext('2d');
@@ -22,10 +20,14 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    this.addToMap(this.backgroundObject);
+
+    this.ctx.translate(this.camera_x, 0);
+
+    this.addObjectsToMap(this.level.backgroundObject);
     this.addToMap(this.character);
-    this.addObjectsToMap(this.enemies);
-    // this.ctx.drawImage(this.light.img, this.light.x, this.light.y);
+    this.addObjectsToMap(this.level.enemies);
+
+    this.ctx.translate(-this.camera_x, 0);
 
     //  draw is called again and again
     requestAnimationFrame(() => {
@@ -40,17 +42,18 @@ class World {
   }
 
   addToMap(movableObject) {
-    try {
-      this.ctx.drawImage(
-        movableObject.img,
-        movableObject.x,
-        movableObject.y,
-        movableObject.width,
-        movableObject.height
-      );
-    } catch (e) {
-      console.warn('error loading image');
-      console.log('could not load image', this.movableObject.img.src);
+    if (movableObject.otherDirection) {
+      this.ctx.save();
+      this.ctx.translate(movableObject.width, 0);
+      this.ctx.scale(-1, 1);
+      movableObject.x = movableObject.x * -1;
+    }
+    this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+    if (movableObject.otherDirection) {
+      movableObject.x = movableObject.x * -1;
+      this.ctx.restore();
     }
   }
+
+  flipImage(movableObject) {}
 }
